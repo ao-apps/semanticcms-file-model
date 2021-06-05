7f0cb8eae788faf2f601ccfef4f64a18c1335128
AO Industries, Inc <support@aoindustries.com>
2021-06-05 01:41:06 -0500

    Open-source projects moved to "com.aoapps" packages

--- src/main/java/com/semanticcms/file/model/File.java
+++ src/main/java/com/semanticcms/file/model/File.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-model - Files nested within SemanticCMS pages and elements.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,9 +22,9 @@
  */
 package com.semanticcms.file.model;
 
-import com.aoindustries.lang.Strings;
-import com.aoindustries.net.Path;
-import com.aoindustries.util.Tuple2;
+import com.aoapps.hodgepodge.util.Tuple2;
+import com.aoapps.lang.Strings;
+import com.aoapps.net.Path;
 import com.semanticcms.core.model.Element;
 import com.semanticcms.core.model.ResourceRef;
 import com.semanticcms.core.resources.ResourceConnection;
